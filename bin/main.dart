6e1070dca1c8c2903dd59d6c95b8f4db5452ba8e
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-12-10 10:22:31 +0530

    finished

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,45 @@ class Student{
   int score;
 
   Student(this.name, this.score);
+
+}
+printStudentDetails(Student student){
+  print("Name=${student.name}");
+  print("score=${student.score}");
+}
+findMaxScore(List<Student> students){
+  int maxScore=0;
+  int index=0;
+  for(int i=0;i<students.length;i++){
+    if(students[i].score>maxScore){
+       index=i;
+       maxScore=students[i].score;
+    }
+  }
+  print("Max Score=");
+  printStudentDetails(students[index]);
+}
+sortStudentsByScore(List<Student> students){
+
+  for(int i=0;i<students.length-1;i++){
+    for(int j=i+1;j<students.length;j++){
+      if(students[i].score>students[j].score){
+        //swap
+        String tempName=students[j].name;
+        int tempScore=students[j].score;
+        students[j].name=students[i].name;
+        students[j].score=students[i].score;
+        students[i].name=tempName;
+        students[i].score=tempScore;
+      }
+    }
+  }
+ printStudentList(students);
+}
+printStudentList(List<Student> students){
+  for(int i=0;i<students.length;i++){
+    print('${students[i].name} :'+'${students[i].score}');
+  }
 }
 // Challenge  1 - Create 3 Students
 // 1. Ram - 87
@@ -17,7 +56,14 @@ class Student{
 
 // Challenge 4 - Sort the students based on their scores
 
-
 void main() {
-
+  Student Ram=Student("Ram",87);
+  Student Shyam=Student("Shyam",80);
+  Student Ghanshyam= Student("Ghanshyam",91);
+  List<Student> students=[Ram,Shyam,Ghanshyam];
+printStudentDetails(Ram);
+  printStudentDetails(Shyam);
+  printStudentDetails(Ghanshyam);
+  findMaxScore(students);
+  sortStudentsByScore(students);
 }
