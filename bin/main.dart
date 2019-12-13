46e0c263c27f283efcf117a54917fcad3fb97446
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-12-13 11:43:36 +0530

    code refactored

--- bin/main.dart
+++ bin/main.dart
@@ -1,48 +1,32 @@
 import 'dart:io';
+
 class Student {
   String name;
   int score;
 
   Student(this.name, this.score);
+}
 
+printStudentDetails(Student student) {
+  print('Name=${student.name}');
+  print('score=${student.score}');
 }
-printStudentDetails(Student student){
-  print("Name=${student.name}");
-  print("score=${student.score}");
-}
-findMaxScore(List<Student> students){
-  int maxScore=0;
-  int index=0;
-  for(int i=0;i<students.length;i++){
-    if(students[i].score>maxScore){
-       index=i;
-       maxScore=students[i].score;
-    }
-  }
-  print("Max Score=");
-  printStudentDetails(students[index]);
-}
-sortStudentsByScore(List<Student> students){
 
-  for(int i=0;i<students.length-1;i++){
-    for(int j=i+1;j<students.length;j++){
-      if(students[i].score>students[j].score){
-        //swap
-        String tempName=students[j].name;
-        int tempScore=students[j].score;
-        students[j].name=students[i].name;
-        students[j].score=students[i].score;
-        students[i].name=tempName;
-        students[i].score=tempScore;
-      }
-    }
-  }
- printStudentList(students);
+void findMaxScore(List<Student> students) {
+  Student Max;
+  Max = students.reduce((curr, next) => curr.score > next.score ? curr : next);
+  print('Student with Max Score');
+  print('Name=${Max.name}');
+  print('Score=${Max.score}');
 }
-printStudentList(List<Student> students){
-  for(int i=0;i<students.length;i++){
-    print('${students[i].name} :'+'${students[i].score}');
-  }
+
+void sortStudentsByScore(List<Student> students) {
+  students.sort((a, b) => a.score.compareTo(b.score));
+  printStudentList(students);
+}
+
+printStudentList(List<Student> students) {
+  students.forEach((n) => print('Name=${n.name},Score=${n.score}'));
 }
 // Challenge  1 - Create 3 Students
 // 1. Ram - 87
@@ -62,28 +46,26 @@ printStudentList(List<Student> students){
 // 'Completed Task n'
 Future nSecondDelay(int n) async {
   print(" Beginning Task $n");
-  await  Future.delayed(Duration(seconds: n));
-   print("Completed Task $n");
+  await Future.delayed(Duration(seconds: n));
+  print("Completed Task $n");
 }
+
 void nSecondDelayUsingThen(int n) {
-
   print(" Beginning Task $n");
-   Future.delayed(Duration(seconds: n)).then((value){
-     print("Completed Task $n");
-   });
-
+  Future.delayed(Duration(seconds: n)).then((value) {
+    print("Completed Task $n");
+  });
 }
 
 // PART 3
 // Create a new function which does the same thing as previous function but
 // without await keyword . (HINT: use '.then' method)
 
-
 void main() {
-  Student Ram=Student("Ram",87);
-  Student Shyam=Student("Shyam",80);
-  Student Ghanshyam= Student("Ghanshyam",91);
-  List<Student> students=[Ram,Shyam,Ghanshyam];
+  Student Ram = Student("Ram", 87);
+  Student Shyam = Student("Shyam", 80);
+  Student Ghanshyam = Student("Ghanshyam", 91);
+  List<Student> students = [Ram, Shyam, Ghanshyam];
   printStudentDetails(Ram);
   printStudentDetails(Shyam);
   printStudentDetails(Ghanshyam);
